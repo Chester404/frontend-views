d0011900edb9ae0ea693f6dd9a38a7c54068d4b5
chester404 <chestermawuli@gmail.com>
2020-12-02 18:47:23 +0000

    Back to working on the profile catalog

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,15 @@ export default function index(){
     return(
         <>
         <Header/>
-        <div className="index_background">This repo has design view of various test pages in Next.js
+        <div className="row index_background">This repo has design view of various test pages in Next.js
             <div className="catalog_background">
-                <div className="innerCatalog">
-
+                <div className="innerCatalog">    
+                </div>
+                <div className="catalogdata">
+                        <p className="catalogTitle_txt">Product Catalog</p>
+                        <p className="catalogYear_txt">2020</p>
+                        <p className="catalogauthor_txt">by Kofi</p>
+                        <p className="catalog_txt">A modern and clean<br/> Product Catalog to<br/> present your<br/> Company's product</p>
                 </div>
             </div>
         </div>
